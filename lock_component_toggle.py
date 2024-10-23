f55f037606dd38c3301415f9c243539bd52591af
dhalonen <danhalonen123@gmail.com>
2024-10-23 10:03:59 -0400

    Toggle Read-Only of workpart

---
+++ lock_component_toggle.py
@@ -0,0 +1,20 @@
+﻿import NXOpen
+import NXOpen.UF
+import os
+
+def main():
+    theSession = NXOpen.Session.GetSession()
+    workPart = theSession.Parts.Work
+    msgbox = NXOpen.UF.UFSession.GetUFSession()
+
+    part = workPart.FullPath
+
+    if os.access(part, os.W_OK):
+        os.chmod(part, 0o444)
+        msgbox.Ui.DisplayMessage("File changed to: Read Only", 0)
+    else:
+        os.chmod(part, 0o666)
+        msgbox.Ui.DisplayMessage("File changed to: Writable", 0)
+
+if __name__ == "__main__":
+    main()
